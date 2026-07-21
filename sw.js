f3f7e6a5abc6fa85bb181b249ade40cb57eb0ceb
magopavlov <132511773+magopavlov@users.noreply.github.com>
2026-07-21 19:48:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
+const CACHE = "rutina-v1";
 
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(["./", "./index.html"]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const network = fetch(e.request)
+        .then((res) => {
+          if (e.request.url.startsWith(self.location.origin)) {
+            caches.open(CACHE).then((cache) => cache.put(e.request, res.clone()));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
